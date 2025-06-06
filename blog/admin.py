1d86b792a04f99823302959299c36e03a2fd6df3
iahia <iahiaimon555@gmail.com>
2025-06-06 23:53:49 +0600

    Creating View

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import CustomUser , Post
+from .models import CustomUser, Post
 
 # Register your models here.
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "username", "created_at")
+    list_filter = ("username", "created_at")
+    search_fields = ("title", "content")
+    raw_id_fields = ("username",)  # Use if too many users
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user":
+            kwargs["queryset"] = CustomUser.objects.all()
+            print("Users in dropdown:", kwargs["queryset"])  # Debug
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
