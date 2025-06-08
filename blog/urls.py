b450fd47d2e76705905bbd3614495ef2700dbf1f
iahia <iahiaimon555@gmail.com>
2025-06-08 23:51:06 +0600

    Todo App

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostApiView 
-
-urlpatterns = [
-    path('posts/' , PostApiView.as_view() , name="posts"),
-    # path('user/' , UserApiView.as_view() , name="user")
-]
